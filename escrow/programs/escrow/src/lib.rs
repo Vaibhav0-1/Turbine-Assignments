4edd72a9f9cff3c555477eecf8b10cca98e967f5
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-04-11 22:47:09 +0530

    escrow

---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,22 @@
+// #![allow(unexpected_cfgs)]
+// use anchor_lang::prelude::*;
+
+// pub mod instructions;
+// pub mod state;
+
+// pub use instruction::*;
+// pub use state::*;
+
+// declare_id!("5WkR49EQboLeeK84G6oJj3AsUHT3zASrqDszCuwky863");
+
+// #[program]
+// pub mod escrow {
+//     use super::*;
+
+//     pub fn maker(ctx: Context<Maker>, seed:u64, deposit: u64, receive:u64) -> Result<()> {
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct Initialize {}
